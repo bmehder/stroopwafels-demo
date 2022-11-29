94d7f54b8705349c2c2be1660eb44f5d59965c5b
bmehder <bmehder@gmail.com>
2022-11-29 14:21:58 +0100

    work on blog

--- src/routes/blog/Post.svelte
+++ src/routes/blog/Post.svelte
@@ -2,27 +2,41 @@
   import type { WP_REST_API_Post } from 'wp-types'
 
   export let post: WP_REST_API_Post
+
+  const src = post._embedded?.['wp:featuredmedia'][0].source_url
 </script>
 
-<div class="container">
-  <h2>
-    <a href="/blog/{post.slug}" data-sveltekit-prefetch
-      >{@html post.title.rendered}</a
-    >
-  </h2>
-  <div class="clamp2">{@html post.excerpt.rendered}</div>
+<div class="card">
+  <a href="/blog/{post.slug}" data-sveltekit-prefetch
+    ><img {src} loading="lazy" alt={post.title.rendered} /></a
+  >
+  <div class="content">
+    <h2>
+      <a href="/blog/{post.slug}" data-sveltekit-prefetch
+        >{@html post.title.rendered}</a
+      >
+    </h2>
+    <div class="clamp">{@html post.excerpt.rendered}</div>
+  </div>
 </div>
 
 <style>
-  .container {
-    padding: var(--spacing);
+  .card {
     box-shadow: var(--shadow);
   }
-  .clamp2 {
+  .content {
+    padding: var(--spacing);
+  }
+  .clamp {
     --numberOfLines: 4;
     display: -webkit-box;
     -webkit-box-orient: vertical;
     -webkit-line-clamp: var(--numberOfLines);
     overflow: hidden;
+  }
+  img {
+    width: 100%;
+    aspect-ratio: 1;
+    object-fit: cover;
   }
 </style>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -5,7 +5,20 @@
   export let data: PageData
 
   const post = data.post[0]
+  const src = post._embedded?.['wp:featuredmedia'][0].source_url
 </script>
 
-<h1>{@html post.title.rendered}</h1>
-<div>{@html post.content.rendered}</div>
+<article>
+  <img {src} alt={post.title.rendered} />
+  <h1>{@html post.title.rendered}</h1>
+  <div>{@html post.content.rendered}</div>
+</article>
+
+<style>
+  article {
+    padding-block: var(--spacing);
+  }
+  img {
+    width: 100%;
+  }
+</style>
